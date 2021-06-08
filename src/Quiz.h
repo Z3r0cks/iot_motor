f640807f8c48988a00e87a4a8665c60f8909b7c5
Z3r0cks <patrick.kaserer@web.de>
2021-06-08 18:38:13 +0200

    WIP

--- src/Quiz.h
+++ src/Quiz.h
@@ -1,37 +1,40 @@
 #include <Arduino.h>
+#include <Motor.h>
 
 struct Question {
-    uint8_t catogery;
     uint8_t correctAnswer;
 };
 
-enum Phase {
-    DeepSleep,
-    CategoryChoice,
-    QuestionChoice,
-    AnswerQuestion,
-    ResolveAnswer
-};
+enum Phase { DeepSleep, QuestionChoice, AnswerQuestion, ResolveAnswer };
 
 class Quiz {
    private:
+    Motor* motor;
+    uint8_t pinA;
+    uint8_t pinB;
+    uint8_t pinC;
+    uint8_t pinD;
+    uint8_t pinDataIn;
+    uint8_t pinDataOut;
+    bool isAtStart;
     bool isGameMaster;
     Phase phase;
     int8_t score;
     int8_t oppositeScore;
-    uint8_t category[3];
-    Question questions[12];
-    uint8_t currentCategory;
-    uint8_t currentQuestion;
-    void chooseCategory();
+    uint8_t* questions;
+    int8_t currentQuestion;
+    void deepSleep();
     void chooseQuestion();
-    
+
    public:
-    Quiz(uint8_t pinA,
+    Quiz(Motor* motor,
+         uint8_t pinA,
          uint8_t pinB,
          uint8_t pinC,
          uint8_t pinD,
-         uint8_t pinData);
+         uint8_t pinDataIn,
+         uint8_t pinDataOut,
+         bool isGameMaster);
     void wakeUp();
     void run();
 };
